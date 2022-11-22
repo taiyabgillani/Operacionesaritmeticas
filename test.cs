aa17d754c8092c6125db03337a4fd9308eb588ff
Camilo María <camilo_jose08@hotmail.com>
2022-11-22 19:39:02 -0400

    .

---
+++ test.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ope;
+
+
+namespace PruebaUnitaria
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void PruebaAreaCuadrado()
+        {
+            int L1 = 2;
+
+            int Esperado = 4;
+
+            double resultado = Clases.AreaCuadrado(L1);
+
+            Assert.AreEqual(Esperado, resultado);
+        }
+
+        [TestMethod]
+        public void PruebaAreaRectangulo()
+        {
+            int Base1 = 2;
+            int Altura1 = 1;
+
+            int Esperado = Base1 * Altura1;
+
+            double resultado = Clases.AreaRectangulo(Base1, Altura1);
+
+            Assert.AreEqual(Esperado, resultado);
+        }
+
+        [TestMethod]
+        public void PruebaAreaTrapecio()
+        {
+            int Base3 = 2;
+            int x = 1;
+            int Altura3 = 2;
+
+            int Esperado = (Base3 + x) * Altura3 / 2;
+
+            double resultado = Clases.AreaTrapecio(Base3, x, Altura3);
+
+            Assert.AreEqual(Esperado, resultado);
+        }
+
+
+    }
+}
